596eab6bed11790b3987cff36e8a38fe0e0b3e6e
Luis Raga <whary11@gmail.com>
2019-02-20 11:18:24 -0500

    conectando con la db...

---
+++ js/db.config.js
@@ -0,0 +1,10 @@
+
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyCBmTdh3gfy4SRrNxDXOmV53rpk43NtQ90",
+    authDomain: 'enredes-f8244.firebaseapp.com',
+    projectId: 'enredes-f8244'
+  });
+  
+  // Initialize Cloud Firestore through Firebase
+  var db = firebase.firestore();
--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
 (function () {
+
+
+    var sendMessage = function (text) {
+        var $messages, message;
+        if (text.trim() === '') {
+            return;
+        }
+        $('.message_input').val('');
+        $messages = $('.messages');
+        // Validar a qué lado se mostrará el mensaje, la primer posibilidad puede ser con el nombre que introduzca el usuario
+        var message_side = message_side === 'left' ? 'right' : 'left';
+        message = new Message({
+            text: text,
+            message_side: message_side
+        });
+        message.draw();
+        return $messages.animate({
+            scrollTop: $messages.prop('scrollHeight')
+        }, 300);
+    };
+
     var Message;
     Message = function (arg) {
         this.text = arg.text, this.message_side = arg.message_side;
+
         this.draw = function (_this) {
             return function () {
                 var $message;
@@ -15,6 +37,10 @@
         }(this);
         return this;
     };
+
+
+
+
     $(function () {
         var getMessageText, message_side, sendMessage;
         message_side = 'right';
@@ -23,6 +49,51 @@
             $message_input = $('.message_input');
             return $message_input.val();
         };
+
+        enviarMensaje = function (data) {
+            db.collection("chat").add({
+                    nombre: data.nombre,
+                    fecha: new Date,
+                    mensaje: data.mensaje
+                })
+                .then(function (docRef) {
+                    console.log("Document written with ID: ", docRef.id);
+                })
+                .catch(function (error) {
+                    console.error("Error adding document: ", error);
+                });
+        }
+
+
+
+
+
+        guardarMessage = function () {
+            var $messages, message;
+
+            var data = {
+                nombre: 'Luis Raga',
+                mensaje: $('.message_input').val()
+            }
+
+            message_side = message_side === 'left' ? 'right' : 'left';
+
+            enviarMensaje(data)
+            // return $messages.animate({
+            //     scrollTop: $messages.prop('scrollHeight')
+            // }, 300);
+        };
+
+
+
+
+
+
+
+
+
+
+
         sendMessage = function (text) {
             var $messages, message;
             if (text.trim() === '') {
@@ -36,40 +107,79 @@
                 message_side: message_side
             });
             message.draw();
-            return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+            return $messages.animate({
+                scrollTop: $messages.prop('scrollHeight')
+            }, 300);
         };
         $('.send_message').click(function (e) {
-            return sendMessage(getMessageText());
+            return guardarMessage();
         });
         $('.message_input').keyup(function (e) {
             if (e.which === 13) {
                 return sendMessage(getMessageText());
             }
         });
-        sendMessage('Hola Raga! :)');
-        setTimeout(function () {
-            return sendMessage('Hola Sara ¿Cómo estás?');
-        }, 1000);
-        return setTimeout(function () {
-            return sendMessage('Estoy bien gracias!');
-        }, 2000);
+
+        // Leer mensajes de la db y dejar escucha de cambios
+        db.collection("chat").orderBy("fecha", "asc").onSnapshot((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                console.log(doc.data());
+                setTimeout(function () {
+                    return sendMessage(doc.data().mensaje);
+                }, 1000);
+            });
+        });
     });
+
+
+
+
+
+
+
+    sendMessage('jkbasjdb')
+
 }.call(this));
 
 
 
-$('.maximize').click(()=>{
+
+
+
+$('.maximize').click(() => {
     $('#control').removeClass('hide')
     $('#control').addClass('show')
     $('.title').text('Chat')
     $('.title').css({
-        fontSize:'15px'
+        fontSize: '15px'
     })
 })
 
-$('.close').click(()=>{
+$('.close').click(() => {
     $('#control').removeClass('show')
     $('#control').addClass('hide')
     $('.title').text('Estamos en Linea....')
 
 })
+
+leerMensajes();
+
+
+function enviarMensaje(data) {
+    db.collection("chat").add({
+            nombre: data.nombre,
+            fecha: new Date,
+            mensaje: data.mensaje
+        })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+}
+
+
+function leerMensajes() {
+
+}
